feee62c0773bf23252abaebd5fa57bc5f852fb99
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 <a1793138@student.adelaide.edu.au>
2022-10-27 15:26:50 +1030

    li

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -3,11 +3,5 @@
 using namespace std;
 int main ()
 {
-    Employee *q[2];
-    for (int i =0;i<2;i++)
-    {
-        q[i]->set_payRate(i);
-        q[i]->set_energyLevel(i);
-    }
     return 0;
 }
